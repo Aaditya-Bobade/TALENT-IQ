dbda27667b3740f1fc7a817271e6134206bb20fb
Aaditya Bobade <aadityabobade17@gmail.com>
2026-01-13 12:44:10 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,22 +1,8 @@
 import axios from "axios";
-import { getToken } from "@clerk/clerk-react";
 
 const axiosInstance = axios.create({
-  baseURL: "https://talent-iq-backend-vkpm.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true, 
 });
-
-// Attach Clerk JWT to every request
-axiosInstance.interceptors.request.use(
-  async (config) => {
-    const token = await getToken();
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
 
 export default axiosInstance;
