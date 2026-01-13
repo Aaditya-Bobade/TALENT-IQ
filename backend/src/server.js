d70e700362cf1f16fed6162b7134fec86a9b1517
Aaditya Bobade <aadityabobade17@gmail.com>
2026-01-13 13:25:35 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true, methods: ["GET", "POST", "PUT", "DELETE"] , allowedHeaders: ["Content-Type", "Authorization"]}));
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(clerkMiddleware()); 
 
 
