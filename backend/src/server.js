5e70a29c5f6355dc426284448603547e97be376d
Aaditya Bobade <aadityabobade17@gmail.com>
2026-01-13 12:46:23 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ENV.CLIENT_URL,
+    origin: "https://talent-iq-78m8.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
